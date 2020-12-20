ff1db1740326b7ce2e43d4d31553cc533f9c89ec
Misbah Ahmed <72851859+skmisbah@users.noreply.github.com>
2020-12-20 11:14:48 +0600

    Simple Sum [Beginner]

---
+++ uri_1003.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+
+    int A{0};
+    int B{0};
+    std::cin >> A;
+    std::cin >> B;
+
+    int SOMA{A + B};
+
+    std::cout << "SOMA = " << SOMA << std::endl;
+
+    return 0;
+}
