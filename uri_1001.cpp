24c3ca00ea5804d6cf962e2cbe8976b35f0a9cf4
Misbah Ahmed <72851859+skmisbah@users.noreply.github.com>
2020-12-20 11:12:45 +0600

    Extemely Basic [Beginner]

---
+++ uri_1001.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+
+    int A{0};
+    int B{0};
+    int X{0};
+
+    std::cin >> A ;
+    std::cin >> B ;
+
+    X = A + B ;
+
+    std::cout << "X = " << X << std::endl;
+
+    return 0;
+}
